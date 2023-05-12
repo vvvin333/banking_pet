9fa1a2c8d43782395d9ada91658e9ccf76a3853b
vvvin333 <333vvvin@gmail.com>
2023-05-12 14:31:28 +0500

    test: combine tests with mark.parametrize

--- tests/unit/clients/test_clients.py
+++ tests/unit/clients/test_clients.py
@@ -1,4 +1,4 @@
-from clients.models import Client
+import pytest
 
 
 def test_get_clients(client, db, bake_clients):
@@ -15,13 +15,19 @@ def test_get_clients(client, db, bake_clients):
         assert response_client["account"] == 100
 
 
-def test_pay_from_client(client, db, bake_clients):
+@pytest.mark.parametrize(
+    "amount, expected",
+    (
+        (30.30, (100 - 30.30, 100 + 10.10)),
+        (10., (100 - 9.99, 100 + 3.33)),
+    ),
+)
+def test_pay_from_client(client, db, bake_clients, amount, expected):
     client_num = 4
     bake_clients(client_num)
 
     from_ptn = 0
     to_ptn = [1, 2, 3]
-    amount = 30.30
     response = client.post(
         "/api/clients/transact",
         data={
@@ -33,35 +39,10 @@ def test_pay_from_client(client, db, bake_clients):
     assert response.status_code == 200
     response_json = response.json()
 
+    donor_result_account, recipient_result_account = expected
     assert response_json[from_ptn]["personal_tax_number"] == 0
-    assert response_json[from_ptn]["account"] == 100 - amount
+    assert response_json[from_ptn]["account"] == donor_result_account
 
     for i in to_ptn:
         assert response_json[i]["personal_tax_number"] == i
-        assert response_json[i]["account"] == 100 + amount/len(to_ptn)
-
-
-def test_pay_from_client_round(client, db, bake_clients):
-    client_num = 4
-    bake_clients(client_num)
-
-    from_ptn = 0
-    to_ptn = [1, 2, 3]
-    amount = 10
-    response = client.post(
-        "/api/clients/transact",
-        data={
-            "from_ptn": from_ptn,
-            "to_ptn": ",".join(map(str, to_ptn)),
-            "amount": amount,
-        },
-    )
-    assert response.status_code == 200
-    response_json = response.json()
-
-    assert response_json[from_ptn]["personal_tax_number"] == 0
-    assert response_json[from_ptn]["account"] == 100 - 9.99
-
-    for i in to_ptn:
-        assert response_json[i]["personal_tax_number"] == i
-        assert response_json[i]["account"] == 100 + 3.33
+        assert response_json[i]["account"] == recipient_result_account
